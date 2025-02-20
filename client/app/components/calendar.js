15ee4ef448f22cd298991f15f7a04a0c3e65046a
chr <a@example.com>
2025-02-19 19:39:26 -0600

    added bookings view

--- client/app/components/calendar.js
+++ client/app/components/calendar.js
@@ -7,17 +7,20 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import { inject as service } from "@ember/service";
 import gql from "graphql-tag";
+import getBookingsQuery from 'peek-client/gql/queries/get_bookings.graphql';
 
 export default class CalendarComponent extends Component {
   @service apollo; 
 
   @tracked calendar;
   @tracked showBookingForm = false; 
+  @tracked showBookingsModal = false;
   @tracked bookingDetails = { first_name: "", last_name: "", event_id: null };     
+  @tracked event_id = null;
+  @tracked bookings = [];
   @tracked currentView = 'timeGridDay'; // Default to Day View
 
 
- // GraphQL mutation for creating a booking
   createBookingMutation = gql`
     mutation create_booking($first_name: String!, $last_name: String!, $event_id: ID!) {
       create_booking(first_name: $first_name, last_name: $last_name, event_id: $event_id) {
@@ -43,12 +46,14 @@ export default class CalendarComponent extends Component {
 
   @action
   handleEventClick(info) {
+    this.event_id = info.event.id;
     this.bookingDetails = {
       first_name: info.event.first_name,
       last_name: info.event.last_name,
       event_id: info.event.id,
     };
-    this.showBookingForm = true;
+    this.fetchBookings()
+    this.openBookingsModal()
   }
 
   @action
@@ -84,6 +89,13 @@ export default class CalendarComponent extends Component {
   }
 
   @action
+  cancelModals() {
+    this.showBookingForm = false;
+    this.showBookingsModal = false;    
+    this.bookingDetails = {};
+  }
+
+  @action
   cancelBooking() {
     this.showBookingForm = false;
     this.bookingDetails = {};
@@ -93,6 +105,39 @@ export default class CalendarComponent extends Component {
   switchView(viewType) {
     this.currentView = viewType;
     this.calendar.changeView(viewType); // FullCalendar's built-in method
+  }
+
+  // Show the booking form modal
+  @action
+  openBookingForm() {
+    this.showBookingForm = true;
+    this.showBookingsModal = false;
+  }
+
+// Show the booking form modal
+  @action
+  openBookingsModal() {
+    this.showBookingForm = false;
+    this.showBookingsModal = true;
+  }
+
+  @action
+  async fetchBookings() {
+    if (!this.event_id) return;
+
+    try {
+        const result = await this.apollo.query({
+          query: getBookingsQuery,
+          variables: { event_id: this.event_id },
+          fetchPolicy: 'network-only', 
+        });
+
+        console.log(result)
+        this.bookings = result.get_bookings_by_event_id || [];
+    } catch (error) {
+        console.error("GraphQL Query Error:", error);
+        throw error; 
+    }
   }
 
 }
